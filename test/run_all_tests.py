043656e61736bb279aecabb6e6ed38d9154db487
eliarizzetto <elia.rizzetto@gmail.com>
2024-04-24 18:11:35 +0200

    Update run_all_tests.py
    run github automated tests in a poetry environment instead of an environment with the base python installation as interpreter

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -2,5 +2,5 @@ from subprocess import Popen
 
 def main():
     Popen(
-        ['python', '-m', 'unittest', 'discover', '-s', 'test', '-p', '*.py', '-b']
+        ['poetry', 'run', 'python', '-m', 'unittest', 'discover', '-s', 'test', '-p', '*.py', '-b']
     )
